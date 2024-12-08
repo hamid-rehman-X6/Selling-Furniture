1a5e60ba5502f15eaa0975bbb92b28ac449cb9dc
hamid-rehman-X6 <hamidrehman1012@gmail.com>
2024-12-08 17:38:57 +0500

    Fixed responsive issues

--- FrontEnd/src/Components/MainPageComponent/MainTitle.jsx
+++ FrontEnd/src/Components/MainPageComponent/MainTitle.jsx
@@ -3,20 +3,24 @@ import React from "react";
 const MainTitle = () => {
   return (
     <>
-      <div className="min-w-max mt-10 font-montserrat">
-        <h1 className="flex items-center justify-center text-sm sm:text-xl  md:text-2xl font-bold tracking-wide">
-          COMPLETE OFFICE FURNITURE SOLUTION IN{" "}
-          <span className="text-[#DE6422] ml-2">DUBAI</span>{" "}
+      <div className="mt-10 font-montserrat">
+        <h1 className="flex flex-wrap items-center justify-center text-sm sm:text-base md:text-xl lg:text-2xl font-bold tracking-wide">
+          COMPLETE OFFICE FURNITURE SOLUTION IN
+          <span className="text-[#DE6422] ml-2">DUBAI</span>
         </h1>
 
-        <p className="text-[12px] flex justify-center mt-6 ">
+        <p className="text-[12px] flex justify-center mt-6">
           OFFICE DESK | CHAIRS | CABINET
         </p>
       </div>
 
-      <div className="mt-14 min-w-full md:min-w-full">
+      <div className="mt-14 w-full">
         <div className="mx-8">
-          <img src="/images/MainPageImg.jpeg" alt="MainPageImage" />
+          <img
+            src="/images/MainPageImg.jpeg"
+            alt="MainPageImage"
+            className="w-full h-auto"
+          />
         </div>
       </div>
     </>
