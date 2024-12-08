ebddd746b002612dcc52a580547985fda1f1265e
hamid-rehman-X6 <hamidrehman1012@gmail.com>
2024-12-08 21:55:38 +0500

    resolve error

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -14,14 +14,14 @@ app.use(cookieParser());
 // CORS configuration
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: "https://localhost:5173",
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true,
     })
 );
 
 // use the user router here
-app.use('auth/users', userRoutes);
+app.use('/auth/users', userRoutes);
 
 app.use(handleError);
 
